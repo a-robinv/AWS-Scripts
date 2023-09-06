80a05eb3da8d86c8f400134194e879871e3f67b6
a-robinv <90445779+a-robinv@users.noreply.github.com>
2023-09-06 18:12:20 +0800

    upload ecs alarms and standardized awk on old scripts

--- Baselining/CreateLB ALarms/lbcreate5xx.sh
+++ Baselining/CreateLB ALarms/lbcreate5xx.sh
@@ -1,5 +1,16 @@
 read -p "Enter Priority, P3 for stg, P2 for prd " PRIORITY
 
+# Generate ARN of LBs
+aws elbv2 describe-load-balancers --region ap-southeast-1 --query "LoadBalancers[].LoadBalancerArn" --output text \
+| awk '{ for (i=1; i<=NF; i++) printf "%s\n", $i }' > arnlist.txt
+
+# string manip
+
+sed -i 's/arn:aws:elasticloadbalancing:ap-southeast-1:811348665611:loadbalancer\///g' arnlist.txt
+
+#For custom Priorities for stg and prd environment, use grep to differentiate the arnlist, and run by environment
+
+#starts cw alarms screation
 while read LB;
 do aws cloudwatch put-metric-alarm \
     --alarm-name "JFC-DT - $PRIORITY - Critical Sum of 5xx errors on $LB" \
@@ -16,4 +27,4 @@ do aws cloudwatch put-metric-alarm \
     --alarm-actions arn:aws:sns:ap-southeast-1:811348665611:ECV-MSP-Support \
     --unit Count \
     --region ap-southeast-1;
-done < lblist.txt;
+done < arnlist.txt;
